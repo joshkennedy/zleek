1a4bc90873f9f79f915121cfc6ac1fb0dad79ec6
Josh Kennedy <jshknndy@gmail.com>
2013-02-20 15:52:17 +0800

    added image loader and sexiness

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,18 @@ $(document).ready(function() {
   $('a.menu').click(function() {
     $('.site-header nav').slideToggle(100);
   });
+  
+  $('')
+
+});
+
+$(function(){
+  $('<img>').attr('src',function(){
+      var imgUrl = $('div.featured-image').css('background-image');
+      imgUrl = imgUrl .substring(4, imgUrl .length-1);
+      return imgUrl;
+  }).load(function(){
+    $('img.loading').fadeOut(500);
+    $('div.overlay').fadeTo("slow", 0.6);
+  });
 });

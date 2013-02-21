a0cb6af8050416f3d40a7c2c5076ca12412bc863
Josh Kennedy <jshknndy@gmail.com>
2013-02-21 17:40:57 +0800

    edit the right files, joshua

--- 404.php
+++ 404.php
@@ -1,9 +1,19 @@
 <?php theme_include('header'); ?>
 
-	<section class="content wrap">
-		<h1>Page not found</h1>
-
-		<p>Unfortunately, the page <code>/<?php echo current_url(); ?></code> could not be found. Your best bet is either to try the <a href="<?php echo base_url(); ?>">homepage</a>, try <a href="#search">searching</a>, or go and cry in a corner (although I don’t recommend the latter).</p>
-	</section>
+ <section class="article">
+  <img src="<?php echo theme_url('/img/loading.gif'); ?>" class="loading" alt="Loading...">
+  <div class="overlay"></div>
+  <div class="featured-image" style="background-image: url(http://fc00.deviantart.net/fs43/f/2009/160/3/a/Burned_Cedar_Plank_by_snakstock.jpg); ?>);"></div>
+  <article class="wrap post">
+    <header class="post-header">
+      <hgroup>
+        <p class="category">Ruh roh</p>
+        <h1>Page Not Found</h1>
+        <p class="lead">Unfortunately, the page <code>/<?php echo current_url(); ?></code> could not be found.</p>
+      </hgroup>
+    </header>
+    <p>Your best bet is either to try the <a href="<?php echo base_url(); ?>">home page</a> or go and cry in a corner (although I don't recommend the latter).</p>
+  </article>
+</section>
 
 <?php theme_include('footer'); ?>
--- page.php
+++ page.php
@@ -29,15 +29,11 @@
 
 <?php else: ?>
 
-    <section class="content wrap">
-      <h1><?php echo page_title(); ?></h1>
-
-      <?php echo page_content(); ?>
-
-      <!--
-        If you want to add custom functionality per-page, you can do so by using the bind() function, which is stored in the functions.php of your theme.
-        Then, if you want to echo out your custom function, you just add a receive() function. Here's an example: "<?php echo receive(); ?>".
-       -->
+    <section class="article archive">
+      <article class="wrap post">
+        <h1><?php echo page_title(); ?></h1>
+        <?php echo page_content(); ?>
+      </article>
     </section>
 
 <?php endif; ?>
